aa80a9f76e3b2067b71824fd32fded8a1954b885
hiepluong2205@gmail.com <Hiep.Luong@coxautoinc.com>
2022-01-11 23:37:01 -0800

    initial

---
+++ src/main/java/io/github/hiepluong2205/App.java
@@ -0,0 +1,13 @@
+package io.github.hiepluong2205;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
